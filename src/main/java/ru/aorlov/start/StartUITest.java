d7b739e6470127a19a976c7560a3fc13a7854d8d
Андрей Орлов <orlov@iteko.com>
2018-04-05 18:37:47 +0300

    1. Используя класс ConsoleInput в классе StartUI обеспечить полноценную работу приложения.

--- src/main/java/ru/aorlov/start/StartUITest.java
+++
@@ -1,9 +0,0 @@
-package ru.aorlov.start;
-
-public class StartUITest {
-
-    public static void main(String[] args) {
-        Input input = new StubInput(new String[] {"create stub task"});
-        new StartUI(input, new Tracker()).init();
-    }
-}
