8f4e44f76a14b049f6ee8b39f638e99229617381
andreworlov91 <Catrobin666>
2018-08-13 15:23:07 +0300

    2. Рефакторинг - Шаблон Декоратор для валидатора.

--- src/main/java/ru/aorlov/start/StubInput.java
+++ src/main/java/ru/aorlov/start/StubInput.java
@@ -1,14 +1,58 @@
 package ru.aorlov.start;
 
-public class StubInput extends ValidateInput {
+import java.util.List;
+
+public class StubInput implements Input {
+
+    /**
+     * The answer field of the array.
+     */
     private String[] answers;
+
+    /**
+     * The field position in the array.
+     */
     private int position = 0;
 
+    /**
+     * Constructor of StubInput class.
+     * @param answers string array answers.
+     */
     public StubInput(String[] answers) {
         this.answers = answers;
     }
 
+    /**
+     * The overridden method ask.
+     * @param question question to the user.
+     * @return response .
+     */
+    @Override
     public String ask(String question) {
         return answers[position++];
+    }
+
+    /**
+     * Method for asking questions to users.
+     * @param question String users question.
+     * @param range int range key.
+     * @return user answer.
+     */
+    @Override
+    public int ask(String question, List<Integer> range) {
+        int key = Integer.valueOf(this.ask(question));
+        boolean exist = false;
+        for (int value : range) {
+            if (value == key) {
+                exist = true;
+                break;
+            }
+        }
+        if (exist) {
+            return key;
+        } else {
+            throw new MenuOutException("Out of menu.");
+        }
+
     }
 }
---
+++ src/test/java/ru/aorlov/start/ValidateInputTest.java
@@ -0,0 +1,50 @@
+package ru.aorlov.start;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+/**
+ * //TODO add comments.
+ *
+ * @author Petr Arsentev (mailto:parsentev@yandex.ru)
+ * @version $Id$
+ * @since 0.1
+ */
+public class ValidateInputTest {
+    private final ByteArrayOutputStream mem = new ByteArrayOutputStream();
+    private final PrintStream out = System.out;
+
+    @Before
+    public void loadMem() {
+        System.setOut(new PrintStream(this.mem));
+    }
+
+    @After
+    public void loadSys() {
+        System.setOut(this.out);
+    }
+
+    @Test
+    public void whenInvalidInput() {
+        ValidateInput input = new ValidateInput(
+                new StubInput(new String[] {"invalid", "1"})
+        );
+        List<Integer> range = new ArrayList<>();
+        range.add(1);
+        input.ask("Select", range);
+        assertThat(
+                this.mem.toString(),
+                is(
+                        String.format("Please enter valid data again!%n")
+                )
+        );
+    }
+}
